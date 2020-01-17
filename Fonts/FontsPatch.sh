7b1d636706d64ece6ccce072b79bd5449c076671
unknown <iwowui@outlook.com>
2020-01-17 16:42:08 +0800

    2020-01-17 16:42:08

--- Fonts/FontsPatch.sh
+++ Fonts/FontsPatch.sh
@@ -4,24 +4,22 @@ export PATH=$PATH:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 script_dir=$(cd "$(dirname "$0")";pwd)
 
 if [ -f "$script_dir/ARKai_T.ttf" ]; then
-    if [ ! -f "$script_dir/ARKai_C.ttf" ]; then
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/FRIZQT__.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ARIALN.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/MORPHEUS.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/SKURRI.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/FRIENDS.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/FRIZQT__.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ARIALN.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/MORPHEUS.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/SKURRI.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/FRIENDS.ttf"
 
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ARHei.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ARKai_C.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ARHei.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ARKai_C.ttf"
 
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bLEI00D.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/arheiuhk_bd.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bHEI00M.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bKAI00M.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bLEI00D.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/arheiuhk_bd.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bHEI00M.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bKAI00M.ttf"
 
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ZYHei.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ZYKai_C.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ZYKai_T.ttf"
-        cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bHEI01B.ttf"
-    fi
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ZYHei.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ZYKai_C.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/ZYKai_T.ttf"
+    cp -f "$script_dir/ARKai_T.ttf" "$script_dir/bHEI01B.ttf"
 fi
